f280df67023e215626a01c5267f6c3a0503cbb6b
birhanumeng <eyerusalemmengiste@gmail.com>
2023-05-04 06:03:18 +0000

    cretae user table with enumerate the country

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Create a user table that can run on any database.
+-- The script should not fail
+CREATE TABLE If NOT EXISTS users (  
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
