899ab6005c4e992c53059a1c218aa684fabd1e0f
birhanumeng <berhanumengstie@gmail.com>
2023-05-09 04:24:06 -0700

    return all the documnet in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 """ Lists all documents in a collection """
 
-from pymongo import MongoClinet
-
-
 def list_all(mongo_collection):
     """ Takes pymongo object as an argument and list all ducoment
-        in a collection as list object.
+        in a collection.
         if the pymongo object is empty it should return empty list
     """
-    result = []
-    with MongoClient as clinet:
-        db = client.my_db
-        for doc in db.school.find():
-            result.append[doc]
-    return result
+    documents = mongo_collection.find()
+    if document.length() == 0:
+        return []
+    return documents
