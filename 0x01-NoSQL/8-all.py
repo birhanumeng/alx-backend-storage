e4779ce6960133d04e387ce9b96eb88dd8f06566
birhanumeng <berhanumengstie@gmail.com>
2023-05-09 02:28:40 -0700

    return all the documnet in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,10 +2,9 @@
 """ Lists all documents in a collection """
 
 from pymongo import MongoClinet
-from typing import List
 
 
-def list_all(mongo_collection: pymongo) -> list[]:
+def list_all(mongo_collection):
     """ Takes pymongo object as an argument and list all ducoment
         in a collection as list object.
         if the pymongo object is empty it should return empty list
