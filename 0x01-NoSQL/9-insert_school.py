0f5bccfd2649de9d34b7114153816e5b6faf6fb8
birhanumeng <berhanumengstie@gmail.com>
2023-05-09 06:04:01 -0700

    insert a document in to a collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Inserts a new document in a collection based on kwargs. """
+
+def insert_school(mongo_collection, **kwargs):
+    """ It takes document (python dictionary) object as an argument
+        and insert into mongo collection argument, mongo_collection.
+    """
+    new = mongo_collection.insert(kwargs)
+    return new.inserted_id
