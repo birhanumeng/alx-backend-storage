55a3fc73933a17cf382b8a010ed0fe2c1d128564
birhanumeng <berhanumengstie@gmail.com>
2023-05-09 06:48:58 -0700

    the attribute valive in a document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Changes all topics of a school document based on the name. """
+
+def update_topics(mongo_collection, name, topics):
+    """ Based on the given name as anargument, change the topics
+        of the document in a collection.
+    """
+    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
