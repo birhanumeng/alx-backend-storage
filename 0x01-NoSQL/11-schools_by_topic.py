c90b08b7fa56e4bbca4ee6bced779b1f86f0e2f8
birhanumeng <berhanumengstie@gmail.com>
2023-05-09 07:05:29 -0700

    list all documents based on the specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,6 +5,6 @@ def schools_by_topic(mongo_collection, topic):
     """ Based on the given topic in the argument list all
         all the document.
     """
-    documents = mongo_collection.find({"topics": topics})
+    documents = mongo_collection.find({"topics": topic})
     docs = [doc for doc in documents]
     return docs
